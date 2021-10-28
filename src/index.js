21c24422e5fee5ff7562d52eacd7606486334683
Sergei Sleptsov <sergei.sleptsov@gmail.com>
2021-10-28 20:48:02 +0300

    ninja-keys integration example

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import webpackLogo from '@/images/webpack-logo.svg'
 
 // Test import of styles
 import '@/styles/index.scss'
-
+import 'ninja-keys';
 // Appending to the DOM
 const logo = document.createElement('img')
 logo.src = webpackLogo
@@ -24,3 +24,51 @@ imagePublic.src = '/assets/example.png'
 
 const app = document.querySelector('#root')
 app.append(logo, heading, imageBackground, imagePublic)
+
+
+const hotkeys = [
+  {
+    id: "Home",
+    title: "Open Home",
+    hotkey: "cmd+h",
+    mdIcon: "home",
+    handler: () => {
+      console.log("navigation to home");
+    }
+  },
+  {
+    id: "Open Projects",
+    title: "Open Projects",
+    hotkey: "cmd+p",
+    mdIcon: "apps",
+    handler: () => {
+      console.log("navigation to projects");
+    }
+  },
+  {
+    id: "Theme",
+    title: "Change theme...",
+    mdIcon: "desktop_windows",
+    children: [
+      {
+        id: "Light Theme",
+        title: "Change theme to Light",
+        mdIcon: "light_mode",
+        handler: () => {
+          console.log("theme light");
+        }
+      },
+      {
+        id: "Dark Theme",
+        title: "Change theme to Dark",
+        mdIcon: "dark_mode",
+        keywords: "lol",
+        handler: () => {
+          console.log("theme dark");
+        }
+      }
+    ]
+  }
+];
+const ninja = document.querySelector("ninja-keys");
+ninja.data = hotkeys;
